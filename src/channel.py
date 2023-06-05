14285ae1e1246e5de9833258392282f9ac10801f
Vladimir Semikov <voesem@ya.ru>
2023-06-05 20:12:48 +0300

    домашнее задание 1 выполнено

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,16 @@
+import json
+
+from googleapiclient.discovery import build
+
+# метод os.getenv('YOUTUBE_API_KEY')
+# выдает ошибку google.auth.exceptions.DefaultCredentialsError, поэтому ключ ввожу прямо здесь
+api_key: str = 'AIzaSyASr8OrVL1W1yIxfphOLonE_Y9PMn45ZNg'
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 
 class Channel:
@@ -5,8 +18,9 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        printj(channel)
