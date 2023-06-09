44b330d205f7b0aa0cd9187e92447e837e52fa59
voesem <voesem@gmail.com>
2023-06-09 19:52:54 +0300

    домашнее задание 2 выполнено

--- src/channel.py
+++ src/channel.py
@@ -17,10 +17,40 @@ class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        """Экземпляр инициализируется по id канала. Остальные данные подтягиваются по API."""
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
+        self.__channel_id = channel_id
+        self.title = channel['items'][0]['snippet']['title']
+        self.channel_description = channel['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
+        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.view_count = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         printj(channel)
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    def to_json(self, filename):
+        data = {
+            'channel_id': self.__channel_id,
+            'title': self.title,
+            'channel_description': self.channel_description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count
+        }
+
+        with open(filename, encoding='utf-8', mode='w') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=api_key)
