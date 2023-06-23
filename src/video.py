45ce72c42c0f405d752c8e5839c9da6df5ee6215
Vladimir Semikov <voesem@ya.ru>
2023-06-23 19:58:07 +0300

    домашнее задание 4 доработано (реализовано наследование класса PLVideo от класса Video)

--- src/video.py
+++ src/video.py
@@ -29,9 +29,10 @@ class Video:
         return self.__video_id
 
 
-class PLVideo:
+class PLVideo(Video):
 
     def __init__(self, video_id: str, playlist_id: str):
+        super().__init__(video_id)
         video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                id=video_id
                                                ).execute()
@@ -40,19 +41,14 @@ class PLVideo:
                                                        part='contentDetails,snippet',
                                                        maxResults=50,
                                                        ).execute()
-        self.__video_id = video_id
         self.title = playlist_videos['items'][0]['snippet']['title']
-        self.url = f'https://youtu.be/{self.__video_id}'
+        self.url = f'https://youtu.be/{self.video_id}'
         self.view_count = video_response['items'][0]['statistics']['viewCount']
         self.like_count = video_response['items'][0]['statistics']['likeCount']
         self.__playlist_id = playlist_id
 
     def __str__(self):
         return self.title
-
-    @property
-    def video_id(self):
-        return self.__video_id
 
     @property
     def playlist_id(self):
